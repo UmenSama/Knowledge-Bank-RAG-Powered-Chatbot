241730e6e50f7b2e20a1795755e8955221c71e03
UmenSama <umensamare@gmail.com>
2025-07-05 17:57:53 +0530

    first commit

---
+++ utils/retriever.js
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OpenAIEmbeddings } from "@langchain/openai";
+
+const openAIApiKey = import.meta.env.VITE_OPENAI_API_KEY;
+const sbApiKey = import.meta.env.VITE_SUPABASE_API_KEY;
+const sbUrl = import.meta.env.VITE_SUPABASE_URL_LC_CHATBOT;
+
+const embeddings = new OpenAIEmbeddings({ openAIApiKey })
+
+const client = createClient(sbUrl, sbApiKey)
+
+const vectorStore = new SupabaseVectorStore(embeddings, {
+    client,
+    tableName: 'documents',
+    queryName: 'match_documents'
+})
+
+const retriever = vectorStore.asRetriever()
+
+export { retriever }
